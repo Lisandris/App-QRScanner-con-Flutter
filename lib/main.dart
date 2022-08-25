d88734765a2ed76add980989622dff46cd127743
Lisandris <lis170694@gmail.com>
2022-08-25 23:05:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:qr_scanner/pages/home_page.dart';
+import 'package:qr_scanner/pages/mapa_page.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Scanner',
+      initialRoute: 'Home',
+      routes: {
+        'home': ( _ ) => HomePage(),
+        'mapa': ( _ ) => MapaPage(),
+      },
+      
+    );
+  }
+}
