fd0e8c10a71ffe7317fce2dcf67f972e3b5fd838
Lisandris <lis170694@gmail.com>
2022-09-06 21:24:10 +0200

    Optimizacion de pantallas de Mapas y Direcciones

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/scan_list_provider.dart';
+import '../widgets/scan_tiles.dart';
 
 class MapasPage extends StatelessWidget {
   const MapasPage({Key? key}) : super(key: key);
@@ -9,30 +7,7 @@ class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-   final scanListProvider = Provider.of<ScanListProvider>(context);
-   final scans =scanListProvider.scans;
+   return const ScanTiles(tipo: 'geo');
 
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => Dismissible(
-        key: UniqueKey(),
-        background: Container(
-          color: Color.fromARGB(255, 224, 97, 139),
-        ),
-        onDismissed: (DismissDirection direction ){
-          Provider.of<ScanListProvider>(context, listen: false)
-            .borrarScanPorId(scans[i].id);
-
-        },
-        child: ListTile(
-          leading: Icon( Icons.map, color: Theme.of(context).primaryColor ),
-          title: Text( scans[i].valor),
-          subtitle: Text( scans[i].id.toString()),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Theme.of(context).primaryColor,),
-          onTap: () => print( scans[i].id),
-        ),
-      )
-    );
   }
 }
---
+++ lib/widgets/scan_tiles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_scanner/providers/scan_list_provider.dart';
+
+class ScanTiles extends StatelessWidget{
+
+  final String tipo;
+
+  const ScanTiles({ required this.tipo });
+
+
+  @override
+  Widget build (BuildContext context) {
+
+    final scanListProvider = Provider.of<ScanListProvider>(context);
+    final scans =scanListProvider.scans;
+
+    return ListView.builder(
+      itemCount: scans.length,
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Color.fromARGB(255, 224, 97, 139),
+        ),
+        onDismissed: (DismissDirection direction ){
+          Provider.of<ScanListProvider>(context, listen: false)
+            .borrarScanPorId(scans[i].id);
+        },
+        child: ListTile(
+          leading: Icon( 
+            this.tipo == 'http'
+            ? Icons.home_outlined 
+            : Icons.map_outlined,
+            color: Theme.of(context).primaryColor 
+          ),
+          title: Text( scans[i].valor),
+          subtitle: Text( scans[i].id.toString()),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Theme.of(context).primaryColor,),
+          onTap: () => print( scans[i].id),
+        ),
+      )
+    );
+  }
+}
